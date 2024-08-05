6a6d06028482cdc3906d56131815aa0a188dacda
Renan Leite <renan.carli@hotmail.com>
2024-08-05 20:52:38 -0300

    Primeiro commit

---
+++ src/main/java/com/petshow/petshow/PetshowApplication.java
@@ -0,0 +1,13 @@
+package com.petshow.petshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetshowApplication.class, args);
+	}
+
+}
